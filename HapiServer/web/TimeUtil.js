e4aff8832c8bf35901270693749cc37d81198bc7
Jeremy Faden <faden@cottagesystems.com>
2022-04-09 08:46:12 -0500

    convert more functions using jsweet

--- HapiServer/web/TimeUtil.js
+++ HapiServer/web/TimeUtil.js
@@ -73,6 +73,27 @@ function format9( d ) {
     }
 }
 
+    /**
+     * Rewrite the time using the format of the example time, which must start with
+     * $Y-$jT, $Y-$jZ, or $Y-$m-$d. For example,
+     * <pre>
+     * {@code
+     * from org.hapiserver.TimeUtil import *
+     * print rewriteIsoTime( '2020-01-01T00:00Z', '2020-112Z' ) # ->  '2020-04-21T00:00Z'
+     * }
+     * </pre> This allows direct comparisons of times for sorting. 
+     * This works by looking at the character in the 8th position (starting with zero) of the 
+     * exampleForm to see if a T or Z is present (YYYY-jjjTxxx).
+     *
+     * TODO: there's
+     * an optimization here, where if input and output are both $Y-$j or both
+     * $Y-$m-$d, then we need not break apart and recombine the time
+     * (isoTimeToArray call can be avoided).
+     *
+     * @param exampleForm isoTime string.
+     * @param time the time in any allowed isoTime format
+     * @return same time but in the same form as exampleForm.
+     */
 function reformatIsoTime(exampleForm, time) {
     var c = exampleForm.charAt(8);
     var nn = TimeUtil.isoTimeToArray(TimeUtil.normalizeTimeString(time));
@@ -107,7 +128,44 @@ function reformatIsoTime(exampleForm, time) {
     }
 };
 
+monthNames = {
+    "jan", "feb", "mar", "apr", "may", "jun",
+    "jul", "aug", "sep", "oct", "nov", "dec"
+};
 
+/**
+ * return the English month name, abbreviated to three letters, for the
+ * month number.
+ *
+ * @param i month number, from 1 to 12.
+ * @return the month name, like "Jan" or "Dec"
+ */
+function monthNameAbbrev(i) {
+    return monthNames[i - 1];
+}
+
+/**
+ * return the month number for the English month name, such as "Jan" (1) or
+ * "December" (12). The first three letters are used to look up the number,
+ * and must be one of: "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+ * "Jul", "Aug", "Sep", "Oct", "Nov", or "Dec" (case insensitive).
+ * @param s the name (case-insensitive, only the first three letters are used.)
+ * @return the number, for example 1 for "January"
+ * @throws ParseException when month name is not recognized.
+ */
+function monthNumber(s) {
+    if (s.length() < 3) {
+        throw "need at least three letters";
+    }
+    s = s.substring(0, 3).toLowerCase()
+    for (var i = 0; i < 12; i++) {
+        if (s==monthNames[i]) {
+            return i + 1;
+        }
+    }
+    throw "Unable to parse month"
+}
+    
 DAYS_IN_MONTH = [[0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31, 0], 
     [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31, 0]];
 
@@ -210,3 +268,96 @@ function now() {
         p.getUTCSeconds(), 
         p.getUTCMilliseconds() * 1e6 ];
 }
+
+/**
+ * return the julianDay for the year month and day. This was verified
+ * against another calculation (julianDayWP, commented out above) from
+ * http://en.wikipedia.org/wiki/Julian_day. Both calculations have 20
+ * operations.
+ *
+ * @param {number} year calendar year greater than 1582.
+ * @param {number} month the month number 1 through 12.
+ * @param {number} day day of month. For day of year, use month=1 and doy for day.
+ * @return {number} the Julian day
+ * @see #fromJulianDay(int)
+ */
+function julianDay(year, month, day) {
+    if (year <= 1582) {
+        throw error("year must be more than 1582");
+    }
+    var jd = 367 * year - (7 * (year + ((month + 9) / 12 | 0)) / 4 | 0) 
+            - (3 * (((year + ((month - 9) / 7 | 0)) / 100 | 0) + 1) / 4 | 0) 
+            + (275 * month / 9 | 0) + day + 1721029;
+    return jd;
+};
+
+/**
+ * Break the Julian day apart into month, day year. This is based on
+ * http://en.wikipedia.org/wiki/Julian_day (GNU Public License), and was
+ * introduced when toTimeStruct failed when the year was 1886.
+ *
+ * @see #julianDay( int year, int mon, int day )
+ * @param {number} julian the (integer) number of days that have elapsed since the
+ * initial epoch at noon Universal Time (UT) Monday, January 1, 4713 BC
+ * @return {int[]} a TimeStruct with the month, day and year fields set.
+ */
+function fromJulianDay(julian) {
+    var j = julian + 32044;
+    var g = (j / 146097 | 0);
+    var dg = j % 146097;
+    var c = (((dg / 36524 | 0) + 1) * 3 / 4 | 0);
+    var dc = dg - c * 36524;
+    var b = (dc / 1461 | 0);
+    var db = dc % 1461;
+    var a = (((db / 365 | 0) + 1) * 3 / 4 | 0);
+    var da = db - a * 365;
+    var y = g * 400 + c * 100 + b * 4 + a;
+    var m = ((da * 5 + 308) / 153 | 0) - 2;
+    var d = da - ((m + 4) * 153 / 5 | 0) + 122;
+    var Y = y - 4800 + ((m + 2) / 12 | 0);
+    var M = (m + 2) % 12 + 1;
+    var D = d + 1;
+    var result = (function (s) { var a = []; while (s-- > 0)
+        a.push(0); return a; })(TimeUtil.TIME_DIGITS);
+    result[0] = Y;
+    result[1] = M;
+    result[2] = D;
+    return result;
+};
+
+/**
+ * subtract the offset from the base time.
+ *
+ * @param {int[]} base a time
+ * @param {int[]} offset offset in each component.
+ * @return {int[]} a time
+ */
+function subtract(base, offset) {
+    var result = (function (s) { var a = []; while (s-- > 0)
+        a.push(0); return a; })(TIME_DIGITS);
+    for (var i = 0; i < TIME_DIGITS; i++) {
+        result[i] = base[i] - offset[i];
+    }
+    if (result[0] > 400) {
+        normalizeTime(result);
+    }
+    return result;
+};
+
+/**
+ * add the offset to the base time. This should not be used to combine two
+ * offsets, because the code has not been verified for this use.
+ *
+ * @param {int[]} base a time
+ * @param {int[]} offset offset in each component.
+ * @return {int[]} a time
+ */
+function add(base, offset) {
+    var result = (function (s) { var a = []; while (s-- > 0)
+        a.push(0); return a; })(TimeUtil.TIME_DIGITS);
+    for (var i = 0; i < TimeUtil.TIME_DIGITS; i++) {
+        result[i] = base[i] + offset[i];
+    }
+    TimeUtil.normalizeTime(result);
+    return result;
+};
