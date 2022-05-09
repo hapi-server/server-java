6c0854322e8212d8c23549e8f26a0ba1ca0aa3fe
Jeremy Faden <faden@cottagesystems.com>
2022-05-09 10:13:00 -0500

    make jumbojar in store.  remove extra aopalliance reference that caused error message (but wasn't a problem)

--- SSCWebServer/src/makeSingleJar.sh
+++ SSCWebServer/src/makeSingleJar.sh
@@ -3,7 +3,7 @@ JAVA=/usr/local/jdk1.8.0_181/bin/java
 
 LIB=/home/jbf/ct/hapi/git/server-java/SSCWebServer/lib
 
-string=$LIB/jettison-1.4.1.jar:aopalliance-repackaged-2.6.1.jar:$LIB/aopalliance-repackaged-2.6.1.jar:$LIB/hk2-api-2.6.1.jar:$LIB/hk2-locator-2.6.1.jar:$LIB/hk2-utils-2.6.1.jar:$LIB/jakarta.activation-api-1.2.2.jar:$LIB/jakarta.annotation-api-1.3.5.jar:$LIB/jakarta.inject-2.6.1.jar:$LIB/jakarta.json-1.1.6-module.jar:$LIB/jakarta.json-1.1.6.jar:$LIB/jakarta.json-api-1.1.6.jar:$LIB/jakarta.json.bind-api-1.0.2.jar:$LIB/jakarta.persistence-api-2.2.3.jar:$LIB/jakarta.servlet-api-4.0.3.jar:$LIB/jakarta.validation-api-2.0.2.jar:$LIB/jakarta.ws.rs-api-2.1.6-sources.jar:$LIB/jakarta.xml.bind-api-2.3.3.jar:$LIB/javassist-3.25.0-GA.jar:$LIB/org.osgi.core-6.0.0.jar:$LIB/osgi-resource-locator-1.0.3.jar:$LIB/yasson-1.0.9.jar:$LIB/jakarta.ws.rs-api-2.1.6.jar:$LIB/jersey-client.jar:$LIB/jersey-common.jar:$LIB/jersey-container-servlet-core.jar:$LIB/jersey-container-servlet.jar:$LIB/jersey-hk2.jar:$LIB/jersey-media-jaxb.jar:$LIB/jersey-media-json-binding.jar:$LIB/jersey-media-sse.jar:$LIB/jersey-server.jar:$LIB/sscWebServices-jaxb.jar:$LIB/sscWebServicesJaxRs-client.jar
+string=$LIB/jettison-1.4.1.jar:$LIB/aopalliance-repackaged-2.6.1.jar:$LIB/hk2-api-2.6.1.jar:$LIB/hk2-locator-2.6.1.jar:$LIB/hk2-utils-2.6.1.jar:$LIB/jakarta.activation-api-1.2.2.jar:$LIB/jakarta.annotation-api-1.3.5.jar:$LIB/jakarta.inject-2.6.1.jar:$LIB/jakarta.json-1.1.6-module.jar:$LIB/jakarta.json-1.1.6.jar:$LIB/jakarta.json-api-1.1.6.jar:$LIB/jakarta.json.bind-api-1.0.2.jar:$LIB/jakarta.persistence-api-2.2.3.jar:$LIB/jakarta.servlet-api-4.0.3.jar:$LIB/jakarta.validation-api-2.0.2.jar:$LIB/jakarta.ws.rs-api-2.1.6-sources.jar:$LIB/jakarta.xml.bind-api-2.3.3.jar:$LIB/javassist-3.25.0-GA.jar:$LIB/org.osgi.core-6.0.0.jar:$LIB/osgi-resource-locator-1.0.3.jar:$LIB/yasson-1.0.9.jar:$LIB/jakarta.ws.rs-api-2.1.6.jar:$LIB/jersey-client.jar:$LIB/jersey-common.jar:$LIB/jersey-container-servlet-core.jar:$LIB/jersey-container-servlet.jar:$LIB/jersey-hk2.jar:$LIB/jersey-media-jaxb.jar:$LIB/jersey-media-json-binding.jar:$LIB/jersey-media-sse.jar:$LIB/jersey-server.jar:$LIB/sscWebServices-jaxb.jar:$LIB/sscWebServicesJaxRs-client.jar
 
 IFS=':' read -r -a array <<< "$string"
 
@@ -12,9 +12,9 @@ cd /home/jbf/ct/hapi/git/server-java/SSCWebServer/classes
 
 for element in "${array[@]}"; do
    echo $element
-   jar xvf $element
+   jar xf $element
 done
 
-rsync -av /home/jbf/ct/hapi/git/server-java/SSCWebServer/build/classes/ ./
+rsync -a /home/jbf/ct/hapi/git/server-java/SSCWebServer/build/classes/ ./
 
-jar cvf ../SSCWebReader.jar *
+jar cvf ../store/SSCWebReader.jar *
