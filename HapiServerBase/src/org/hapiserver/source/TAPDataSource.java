cddd671d5b409b54a7672031db31849d75f13681
brownle1 <brownle1@brownle1-wl3.dom1.jhuapl.edu>
2022-05-19 13:47:37 -0400

    ESAC Table Access Protocol data source

---
+++ HapiServerBase/src/org/hapiserver/source/TAPDataSource.java
@@ -0,0 +1,67 @@
+package org.hapiserver.source;
+
+
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.util.Iterator;
+
+import org.hapiserver.AbstractHapiRecordSource;
+import org.hapiserver.HapiRecord;
+import org.hapiserver.TimeUtil;
+
+public class TAPDataSource extends AbstractHapiRecordSource {
+	
+	
+	private String tapServerURL;
+	private String id;
+
+	public TAPDataSource(String tapServerURL,String id) {
+		this.tapServerURL = tapServerURL;
+		this.id = id;
+		
+	}
+
+	@Override
+	public boolean hasGranuleIterator() {
+		return false;
+	}
+
+	@Override
+	public boolean hasParamSubsetIterator() {
+		return false;
+	}
+
+	@Override
+	public Iterator<HapiRecord> getIterator(int[] start, int[] stop) {
+		String startTimeString=formatTime(start);
+		String endTimeString=formatTime(stop);
+		
+		String queryString = tapServerURL+"data?RETRIEVAL_TYPE=product&RETRIEVAL_ACCESS=streamed&DATASET_ID="+id+
+				"&START_DATE="+startTimeString+"&END_DATE="+endTimeString;
+		System.err.println("Querying: "+queryString);
+		try {
+			URL uu = new URL(queryString);
+			InputStream in= uu.openStream();
+			ReadableByteChannel lun= Channels.newChannel(in);
+			CefFileIterator iter = new CefFileIterator(lun);
+	
+			return iter;
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+	}
+
+
+	private String formatTime(int[] time) {
+		String timeString = String.format("%4d-%02d-%02dT%02d:%02d:%02dZ",
+				time[0], time[1], time[2],time[3], time[4], time[5]);
+		return timeString;
+	}
+	
+	
+}
