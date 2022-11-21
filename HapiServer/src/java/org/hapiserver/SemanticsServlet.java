0ff8552c3a60ce4c97af1fafd849178dd98ec17c
Jeremy Faden <faden@cottagesystems.com>
2022-11-21 11:30:18 -0600

    handle semantics response

---
+++ HapiServer/src/java/org/hapiserver/SemanticsServlet.java
@@ -0,0 +1,100 @@
+
+package org.hapiserver;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+
+/**
+ *
+ * @author jbf
+ */
+public class SemanticsServlet extends HttpServlet {
+    
+    private static final Logger logger= Util.getLogger();
+    
+    private String HAPI_HOME;
+    
+    @Override
+    public void init() throws ServletException {
+        super.init(); 
+        HAPI_HOME= getServletContext().getInitParameter("hapi_home");
+        logger.log(Level.INFO, "hapi_home is {0}", HAPI_HOME);
+    }
+    
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("application/json;charset=UTF-8");
+                
+        response.setHeader("Access-Control-Allow-Origin", "* " );
+        response.setHeader("Access-Control-Allow-Methods","GET" );
+        response.setHeader("Access-Control-Allow-Headers","Content-Type" );
+        
+        try {
+            JSONObject json= HapiServerSupport.getSemantics(HAPI_HOME);
+            try (PrintWriter out = response.getWriter()) {
+                String s= json.toString(4);
+                out.write(s);
+            } catch ( JSONException ex ) {
+                throw new ServletException(ex);
+            }
+        } catch ( JSONException ex ) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
