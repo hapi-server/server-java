b09aa8197c3346e247560e6044e726c59f0f6f8b
Jeremy Faden <faden@cottagesystems.com>
2023-03-21 07:08:01 -0500

    add case=5 to study multi-dimensional array operation

--- HapiServerBase/src/org/hapiserver/source/TAPDataSource.java
+++ HapiServerBase/src/org/hapiserver/source/TAPDataSource.java
@@ -119,7 +119,7 @@ public class TAPDataSource extends AbstractHapiRecordSource {
     public static void mainCase4( String[] args ) throws ParseException {
         String tapServerURL="https://csa.esac.esa.int/csa-sl-tap/";
         String id="C4_CP_CIS-CODIF_HS_O1_PEF";
-        String tr= "2021-12-01T00:00/01:00";
+        String tr= "2021-12-01T00:00/00:02";
         int[] timerange = TimeUtil.parseISO8601TimeRange(tr);
         int[] start= Arrays.copyOfRange( timerange, 0, 7 );
         int[] stop= Arrays.copyOfRange( timerange, 7, 14 );
@@ -130,10 +130,27 @@ public class TAPDataSource extends AbstractHapiRecordSource {
         }
     }
     
+    public static void mainCase5( String[] args ) throws ParseException {
+        String tapServerURL="https://csa.esac.esa.int/csa-sl-tap/";
+        String id="C1_CP_PEA_3DRH_PSD";
+        String tr= "2019-08-01T00:00/0:10";
+        int[] timerange = TimeUtil.parseISO8601TimeRange(tr);
+        int[] start= Arrays.copyOfRange( timerange, 0, 7 );
+        int[] stop= Arrays.copyOfRange( timerange, 7, 14 );
+        Iterator<HapiRecord> iter= new TAPDataSource(tapServerURL, id).getIterator(start, stop);
+        while ( iter.hasNext() ) {
+            HapiRecord r= iter.next();
+            System.err.println( String.format( "%s %d fields", r.getIsoTime(0), r.length() ) );
+        }
+    }
+
+            
+        
     public static void main(String[] args ) throws Exception {
         //mainCase1(args);
         //mainCase2(args);
         //mainCase3(args);
         mainCase4(args);
+        //mainCase5(args);
     }
 }
