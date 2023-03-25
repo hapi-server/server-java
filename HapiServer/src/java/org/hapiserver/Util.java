85f232ef1b989f3821987c618e039b58aae034f3
Jeremy Faden <faden@cottagesystems.com>
2023-03-25 12:48:29 -0500

    test to verify deployment

--- HapiServer/src/java/org/hapiserver/Util.java
+++ HapiServer/src/java/org/hapiserver/Util.java
@@ -442,7 +442,7 @@ public final class Util {
      * @return the time this file was last modified.
      */
     public static String buildTime() {
-        String time= "last_modified_at: 2023-03-25T10:48:00";
+        String time= "last_modified_at: 2023-03-25T12:48:00";
         return time.substring(18);
     }
 }
