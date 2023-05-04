18d49517004ac7b564e6a9971709efe964f52aa2
Jeremy Faden <faden@cottagesystems.com>
2023-05-04 12:39:30 -0500

    add int and string support

--- HapiServerBase/src/org/hapiserver/source/cdaweb/CdawebServicesHapiRecordSource.java
+++ HapiServerBase/src/org/hapiserver/source/cdaweb/CdawebServicesHapiRecordSource.java
@@ -5,6 +5,7 @@
  */
 package org.hapiserver.source.cdaweb;
 
+import gov.nasa.gsfc.spdf.cdfj.CDFDataType;
 import gov.nasa.gsfc.spdf.cdfj.CDFException;
 import gov.nasa.gsfc.spdf.cdfj.CDFReader;
 import java.io.File;
@@ -49,6 +50,18 @@ public class CdawebServicesHapiRecordSource implements Iterator<HapiRecord> {
         }
         public double adaptDouble( int index ) {
             return Double.NEGATIVE_INFINITY;
+        }
+        public int adaptInteger( int index ) {
+            return Integer.MIN_VALUE;
+        }
+        public double[] adaptDoubleArray( int index ) {
+            return null;
+        }
+        public int[] adaptIntegerArray( int index ) {
+            return null;
+        }        
+        public String[] adaptStringArray( int index ) {
+            return null;
         }
     }
     
@@ -118,6 +131,70 @@ public class CdawebServicesHapiRecordSource implements Iterator<HapiRecord> {
         }
     }
     
+    private static class DoubleFloatAdapter extends Adapter {
+        float[] array;
+        
+        private DoubleFloatAdapter( float[] array ) {
+            this.array= array;
+        }
+        
+        @Override
+        public double adaptDouble(int index) {
+            return this.array[index];
+        }
+    }
+    
+    private static class IntegerLongAdapter extends Adapter {
+        long[] array;
+        
+        private IntegerLongAdapter( long[] array ) {
+            this.array= array;
+        }
+        
+        @Override
+        public int adaptInteger( int index ) {
+            return (int)this.array[index];
+        }
+    }
+    
+    private static class IntegerIntegerAdapter extends Adapter {
+        int[] array;
+        
+        private IntegerIntegerAdapter( int[] array ) {
+            this.array= array;
+        }
+        
+        @Override
+        public int adaptInteger( int index ) {
+            return this.array[index];
+        }
+    }
+    
+    private static class IntegerShortAdapter extends Adapter {
+        short[] array;
+        
+        private IntegerShortAdapter( short[] array ) {
+            this.array= array;
+        }
+        
+        @Override
+        public int adaptInteger( int index ) {
+            return this.array[index];
+        }
+    }
+    private static class IntegerByteAdapter extends Adapter {
+        byte[] array;
+        
+        private IntegerByteAdapter( byte[] array ) {
+            this.array= array;
+        }
+        
+        @Override
+        public int adaptInteger( int index ) {
+            return this.array[index];
+        }
+    }
+    
     private static class IsotimeTT2000Adapter extends Adapter {
         int julianDay;
         long cdfTT2000= Long.MAX_VALUE;
@@ -167,6 +244,55 @@ public class CdawebServicesHapiRecordSource implements Iterator<HapiRecord> {
             return formatTime( array[index] );
         }
     }
+    
+    /**
+     * Returns the name of the integer data type, for example, 8 is type
+     * 8-byte integer (a.k.a. Java long), and 33 is CDF_TT2000.
+     * @param type the code for data type
+     * @return a name identifying the type.
+     * @see https://spdf.gsfc.nasa.gov/pub/software/cdf/doc/cdf380/cdf38ifd.pdf page 33.
+     */
+    public static String nameForType(int type) {
+        switch (type) {
+            case 1:
+                return "CDF_INT1";
+            case 41:
+                return "CDF_BYTE";  // 1-byte signed integer
+            case 2:
+                return "CDF_INT2"; 
+            case 4:
+                return "CDF_INT4";
+            case 8:
+                return "CDF_INT8";
+            case 11:
+                return "CDF_UINT1";
+            case 12:
+                return "CDF_UINT2";
+            case 14:
+                return "CDF_UINT4";
+            case 21:
+                return "CDF_REAL4";
+            case 44:
+                return "CDF_FLOAT"; 
+            case 22:
+                return "CDF_REAL8";
+            case 45:
+                return "CDF_DOUBLE"; 
+            case 31:
+                return "CDF_EPOCH";
+            case 32:
+                return "CDF_EPOCH16";  // make of two CDF_REAL8,
+            case 33:
+                return "CDF_TT2000";
+            case 51:
+                return "CDF_CHAR";
+            case 52:
+                return "CDF_UCHAR";
+            default:
+                return "???";
+        }
+    }
+    
             
     public CdawebServicesHapiRecordSource(String id, int[] start, int[] stop, String[] params) {
         try {
@@ -223,8 +349,24 @@ public class CdawebServicesHapiRecordSource implements Iterator<HapiRecord> {
                     String param= params[i];
                     int type= reader.getType(param);
                     Object o= reader.get(param);
-                    if ( type==44 ) {
+                    String stype= nameForType(type);
+                    Class c= o.getClass().getComponentType();
+                    if ( c==double.class ) {
                         adapters[i]= new DoubleDoubleAdapter( (double[])o );
+                    } else if ( c==float.class ) {
+                        adapters[i]= new DoubleFloatAdapter( (float[])o );
+                    } else if ( c==int.class ) {
+                        adapters[i]= new IntegerIntegerAdapter( (int[])o );
+                    } else if ( c==short.class ) {
+                        adapters[i]= new IntegerShortAdapter( (short[])o );
+                    } else if ( c==byte.class ) {
+                        adapters[i]= new IntegerByteAdapter( (byte[])o );
+                    } else if ( c==long.class ) {
+                        adapters[i]= new IntegerLongAdapter( (long[])o );
+                    } else if ( stype.equals("CDF_UINT2") ) {
+                        adapters[i]= new IntegerIntegerAdapter( (int[])o );
+                    } else if ( stype.equals("CDF_UINT1") ) {
+                        adapters[i]= new IntegerShortAdapter( (short[])o );                        
                     } else {
                         throw new IllegalArgumentException("unsupported type");
                     }
@@ -260,7 +402,7 @@ public class CdawebServicesHapiRecordSource implements Iterator<HapiRecord> {
 
             @Override
             public String getString(int i) {
-                return null;
+                return adapters[i].adaptString(j);
             }
 
             @Override
@@ -280,7 +422,7 @@ public class CdawebServicesHapiRecordSource implements Iterator<HapiRecord> {
 
             @Override
             public int getInteger(int i) {
-                return Integer.MAX_VALUE;
+                return adapters[i].adaptInteger(i);
             }
 
             @Override
@@ -311,10 +453,10 @@ public class CdawebServicesHapiRecordSource implements Iterator<HapiRecord> {
                 "RBSP-B_DENSITY_EMFISIS-L4", 
                 new int[] { 2019, 7, 15, 0, 0, 0, 0 },
                 new int[] { 2019, 7, 16, 0, 0, 0, 0 }, 
-                new String[] { "Time", "fce" } );
+                new String[] { "Time", "fce", "bmag" } );
         while ( dd.hasNext() ) {
             HapiRecord rec= dd.next();
-            System.err.println( String.format( "%s %.2f", rec.getIsoTime(0), rec.getDouble(1) ) );
+            System.err.println( String.format( "%s %.2f %.2f", rec.getIsoTime(0), rec.getDouble(1), rec.getDouble(2) ) );
         }
     }
     
