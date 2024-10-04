bb37a151a5680013e15ff99b9f1c7f6b7ad423ea
Jeremy Faden <faden@cottagesystems.com>
2024-10-04 09:48:14 -0500

    introduce TimeString class, which may replace the int[] arguments which are difficult to use.

--- HapiServerBase/src/org/hapiserver/TimeString.java
+++ HapiServerBase/src/org/hapiserver/TimeString.java
@@ -22,19 +22,33 @@ public final class TimeString {
         return this.iso8601;
     }
     
+    /**
+     * This could use TimeUtil.isoTimeToArray(iso8601), but since we 
+     * store the times in a prescribed way, we can optimize for performance.
+     * @return [ Y, m, d, H, M, S, N ]
+     * @see TimeUtil#isoTimeToArray(java.lang.String) 
+     */
     public int[] toComponents() {
         char[] cc= iso8601.toCharArray();
         return new int[] { 
-            cc[0]*1000 + cc[1]*100 + cc[2]*10 + cc[3],
-            cc[5]*10 + cc[6],
-            cc[8]*10 + cc[9],
-            cc[11]*10 + cc[12],
-            cc[14]*10 + cc[15],
-            cc[17]*10 + cc[18],
-            cc[20]*100000000 + cc[21]*10000000 + cc[22]*1000000 
-            + cc[23]*100000 + cc[24]*10000 + cc[25]*1000
-            + cc[26]*100 + cc[27]*10 + cc[28] 
+            (cc[0]-48)*1000 + (cc[1]-48)*100 + (cc[2]-48)*10 + (cc[3]-48),
+            (cc[5]-48)*10 + (cc[6]-48),
+            (cc[8]-48)*10 + (cc[9]-48),
+            (cc[11]-48)*10 + (cc[12]-48),
+            (cc[14]-48)*10 + (cc[15]-48),
+            (cc[17]-48)*10 + (cc[18]-48),
+            (cc[20]-48)*100000000 + (cc[21]-48)*10000000 + (cc[22]-48)*1000000 
+            + (cc[23]-48)*100000 + (cc[24]-48)*10000 + (cc[25]-48)*1000
+            + (cc[26]-48)*100 + (cc[27]-48)*10 + (cc[28]-48) 
         };
+    }
+    
+    public static void main( String[] args ) {
+        TimeString r= new TimeString("2043-04-05T23:13:02.123456789");
+        System.err.println(r);
+        for ( int i: r.toComponents() ) {
+            System.err.println(i);
+        }
     }
     
 }
