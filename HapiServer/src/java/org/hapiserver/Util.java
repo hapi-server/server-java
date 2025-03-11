5a39d7b63ce63f022e15c22bb45297f333b78d0e
Jeremy Faden <faden@cottagesystems.com>
2025-03-11 13:06:04 -0500

    figure out bug with AMPTE

--- HapiServer/src/java/org/hapiserver/Util.java
+++ HapiServer/src/java/org/hapiserver/Util.java
@@ -443,7 +443,7 @@ public final class Util {
      * @return the time this file was last modified.
      */
     public static String buildTime() {
-        String time= "last_modified_at: 2025-03-11T09:48";
+        String time= "last_modified_at: 2025-03-11T13:05";
         return time.substring(18);
     }
     
