d4cf67c05f6dcceaa75ac864c21a4a640a689b26
Jeremy Faden <faden@cottagesystems.com>
2025-03-11 09:48:21 -0500

    remove skips.

--- HapiServer/src/java/org/hapiserver/Util.java
+++ HapiServer/src/java/org/hapiserver/Util.java
@@ -443,7 +443,7 @@ public final class Util {
      * @return the time this file was last modified.
      */
     public static String buildTime() {
-        String time= "last_modified_at: 2025-03-11T09:12";
+        String time= "last_modified_at: 2025-03-11T09:48";
         return time.substring(18);
     }
     
