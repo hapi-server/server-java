9b18196b2df79d0c1a223cf829b376bc8ce00bc3
Jeremy Faden <faden@cottagesystems.com>
2025-03-13 09:50:34 -0500

    tweak build time

--- HapiServer/src/java/org/hapiserver/Util.java
+++ HapiServer/src/java/org/hapiserver/Util.java
@@ -443,7 +443,7 @@ public final class Util {
      * @return the time this file was last modified.
      */
     public static String buildTime() {
-        String time= "last_modified_at: 2025-03-13T09:18";
+        String time= "last_modified_at: 2025-03-13T09:50";
         return time.substring(18);
     }
     
