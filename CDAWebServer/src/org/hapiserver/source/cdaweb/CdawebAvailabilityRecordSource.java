8f812a1cee7e12c188e6015d29dd66a7126ac8e4
Jeremy Faden <faden@cottagesystems.com>
2025-03-16 08:06:13 -0500

    rename CDAWebAvailabilitySource to CDAWebAvailabilityRecordSource

--- CDAWebServer/src/org/hapiserver/source/cdaweb/CdawebAvailabilityRecordSource.java
+++ CDAWebServer/src/org/hapiserver/source/cdaweb/CdawebAvailabilityRecordSource.java
@@ -24,7 +24,7 @@ import org.w3c.dom.NodeList;
  * return availability, showing when file granules are found.
  * @author jbf
  */ 
-public class CdawebAvailabilitySource extends AbstractHapiRecordSource {
+public class CdawebAvailabilityRecordSource extends AbstractHapiRecordSource {
 
     private static final Logger logger= Logger.getLogger("hapi.cdaweb");
     
@@ -43,7 +43,7 @@ public class CdawebAvailabilitySource extends AbstractHapiRecordSource {
      * @param idavail the id for the availability set, like "AC_OR_SSC/source"
      * @param info the info for this availability set.
      */
-    public CdawebAvailabilitySource( String availRoot, String idavail, JSONObject info ) {
+    public CdawebAvailabilityRecordSource( String availRoot, String idavail, JSONObject info ) {
         String roots= availRoot + "/" + "info/";
         spid= spidFor(idavail);
         bobwurl= roots + spid + ".json";
@@ -497,7 +497,7 @@ public class CdawebAvailabilitySource extends AbstractHapiRecordSource {
                     throw new RuntimeException(ex);
                 }
                 Iterator<HapiRecord> iter = 
-                        new CdawebAvailabilitySource( orig_data,args[0],info).getIterator( 
+                        new CdawebAvailabilityRecordSource( orig_data,args[0],info).getIterator( 
                                 TimeUtil.parseISO8601Time(args[1]), 
                                 TimeUtil.parseISO8601Time(args[2]) );
                 if ( iter.hasNext() ) {
