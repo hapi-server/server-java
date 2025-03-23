c887ab0eaf429d33a25c2499239fc3926ab99da1
Jeremy Faden <faden@cottagesystems.com>
2025-03-23 11:10:10 -0500

    fix imports

--- CDAWebServer/src/org/hapiserver/source/cdaweb/CdawebInfoCatalogSource.java
+++ CDAWebServer/src/org/hapiserver/source/cdaweb/CdawebInfoCatalogSource.java
@@ -7,31 +7,19 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.text.ParseException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.regex.Pattern;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathExpressionException;
-import javax.xml.xpath.XPathFactory;
 import org.codehaus.jettison.json.JSONArray;
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 import org.hapiserver.TimeUtil;
 import org.hapiserver.source.SourceUtil;
-import org.w3c.dom.Document;
-import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
         
 /**
  * Returns catalog response based on all.xml, and info responses from
