83bfe9ce5bbcc0c671193e65f45ae5ac6440b5ac
Jeremy Faden <faden@cottagesystems.com>
2025-06-14 07:58:49 -0500

    timestamp

--- HapiServer/src/java/org/hapiserver/Util.java
+++ HapiServer/src/java/org/hapiserver/Util.java
@@ -444,7 +444,7 @@ public final class Util {
      * @return the time this file was last modified.
      */
     public static String buildTime() {
-        String time= "last_modified_at: 2025-06-14T07:43";
+        String time= "last_modified_at: 2025-06-14T07:58";
         return time.substring(18);
     }
     
