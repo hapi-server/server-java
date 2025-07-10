7106879c7f84f395800048265bb78a69bc2577d4
Jeremy Faden <faden@cottagesystems.com>
2025-07-10 11:45:32 -0500

    x_groups and x_source is properly supported.

--- HapiServer/src/java/org/hapiserver/HapiServerSupport.java
+++ HapiServer/src/java/org/hapiserver/HapiServerSupport.java
@@ -250,8 +250,8 @@ public class HapiServerSupport {
         for ( int i=0; i<catalog.length(); i++ ) {
             JSONObject item= catalog.getJSONObject(i);
             item= resolveOptions( optionsMap, item );
-            String groupId= item.optString("group_id", item.optString("x_group_id",null) );
-            String source= item.optString("source",item.optString("x_source",null));
+            String groupId= item.optString("group_id", item.optString("x_group_id","") );
+            String source= item.optString("source",item.optString("x_source",""));
             JSONObject config= item.optJSONObject("config");
             if ( config==null ) config= item.optJSONObject("x_config");
             
