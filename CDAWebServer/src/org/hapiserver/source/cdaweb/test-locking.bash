a08aedb623cb36f92ccb4e193e0a33af0b59133f
Jeremy Faden <faden@cottagesystems.com>
2025-07-25 07:48:24 -0500

    allow capabilities, x-landing, and about to come from config.json

---
+++ CDAWebServer/src/org/hapiserver/source/cdaweb/test-locking.bash
@@ -0,0 +1,6 @@
+wget -O 1.csv -o /dev/null 'http://localhost:8280/HapiServer/hapi/data?dataset=AC_OR_SSC&start=2023-01-01T00:00Z&stop=2024-01-01T00:00Z' & 
+wget -O 2.csv -o /dev/null 'http://localhost:8280/HapiServer/hapi/data?dataset=AC_OR_SSC&start=2023-01-01T00:00Z&stop=2024-01-01T00:00Z' & 
+wget -O 3.csv -o /dev/null 'http://localhost:8280/HapiServer/hapi/data?dataset=AC_OR_SSC&start=2023-01-01T00:00Z&stop=2024-01-01T00:00Z' &
+wget -O 4.csv -o /dev/null 'http://localhost:8280/HapiServer/hapi/data?dataset=AC_OR_SSC&start=2023-01-01T00:00Z&stop=2024-01-01T00:00Z' & 
+# wget -O 5.csv -o /dev/null 'http://localhost:8280/HapiServer/hapi/data?dataset=AC_OR_SSC&start=2023-01-01T00:00Z&stop=2024-01-01T00:00Z' & 
+# wget -O 6.csv -o /dev/null 'http://localhost:8280/HapiServer/hapi/data?dataset=AC_OR_SSC&start=2023-01-01T00:00Z&stop=2024-01-01T00:00Z' &
