a53b3bda5925d1b41d4937abca9e0aa5804fca98
Jeremy Faden <faden@cottagesystems.com>
2025-07-31 08:48:28 -0500

    tweak the version number

--- HapiServer/src/java/org/hapiserver/Util.java
+++ HapiServer/src/java/org/hapiserver/Util.java
@@ -444,7 +444,7 @@ public final class Util {
      * @return the time this file was last modified.
      */
     public static String buildTime() {
-        String time= "last_modified_at: 2025-07-10T13:39";
+        String time= "last_modified_at: 2025-07-31T08:47";
         return time.substring(18);
     }
     
