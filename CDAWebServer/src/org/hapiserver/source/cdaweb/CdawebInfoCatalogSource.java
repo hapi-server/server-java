e8f6fb384d265648fd8d23d3aa855fae17e0f549
Jeremy Faden <faden@cottagesystems.com>
2025-08-27 11:05:17 -0500

    tweak version

--- CDAWebServer/src/org/hapiserver/source/cdaweb/CdawebInfoCatalogSource.java
+++ CDAWebServer/src/org/hapiserver/source/cdaweb/CdawebInfoCatalogSource.java
@@ -34,7 +34,7 @@ public class CdawebInfoCatalogSource {
     
     public static final String CDAWeb = "https://cdaweb.gsfc.nasa.gov/";
 
-    private static final String CDAWEB_HAPI_VERSION = "20250614.0743";
+    private static final String CDAWEB_HAPI_VERSION = "20250827.1105";
     
     protected static Map<String,String> coverage= new HashMap<>();
     protected static Map<String,String> filenaming= new HashMap<>();
