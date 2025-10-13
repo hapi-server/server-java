dc554f9d1f56044f2de95a2f92042948728c931a
Jeremy Faden <faden@cottagesystems.com>
2025-10-13 12:40:04 -0500

    tweak time stamp.  (Same time as yesterday, whoa!)

--- HapiServer/src/java/org/hapiserver/Util.java
+++ HapiServer/src/java/org/hapiserver/Util.java
@@ -444,7 +444,7 @@ public final class Util {
      * @return the time this file was last modified.
      */
     public static String buildTime() {
-        String time= "last_modified_at: 2025-10-12T12:39";
+        String time= "last_modified_at: 2025-10-13T12:39";
         return time.substring(18);
     }
     
