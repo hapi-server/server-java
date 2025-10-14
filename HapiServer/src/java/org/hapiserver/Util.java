80b7b527d64f5166fdbe03496050076ed9f38325
Jeremy Faden <faden@cottagesystems.com>
2025-10-14 07:33:51 -0500

    verify that x_format can be used. CDAWeb data?id=L7_K0_SPA&parameters=spa_p_flx had format which did not parse.

--- HapiServer/src/java/org/hapiserver/Util.java
+++ HapiServer/src/java/org/hapiserver/Util.java
@@ -444,7 +444,7 @@ public final class Util {
      * @return the time this file was last modified.
      */
     public static String buildTime() {
-        String time= "last_modified_at: 2025-10-14T07:10";
+        String time= "last_modified_at: 2025-10-14T07:33";
         return time.substring(18);
     }
     
