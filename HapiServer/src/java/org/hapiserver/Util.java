e2bf7aaa7e3612afec7adace48373e68f375c342
Jeremy Faden <faden@cottagesystems.com>
2026-01-30 06:15:32 -0600

    correction to alternate_view pointed out by Bob and Lucy, tweak version

--- HapiServer/src/java/org/hapiserver/Util.java
+++ HapiServer/src/java/org/hapiserver/Util.java
@@ -462,7 +462,7 @@ public final class Util {
      * @return the time this file was last modified.
      */
     public static String buildTime() {
-        String time= "last_modified_at: 2025-10-31T11:03";
+        String time= "last_modified_at: 2026-01-30T06:15";
         return time.substring(18);
     }
     
