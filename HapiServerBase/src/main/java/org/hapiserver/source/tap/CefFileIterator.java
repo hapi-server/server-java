e4ce65fff650d3bbfa8062406b22936cb60d1645
Jeremy Faden <faden@cottagesystems.com>
2026-03-28 07:54:07 -0500

    bugfix: Bob pointed out where 500 error occurs with C1_CT_AUX_ESOC_THRUSTER, where getIsoTime assumed a given length of the times.  Refactor this into its own, easy-to-find class CefHapiRecord.

--- HapiServerBase/src/main/java/org/hapiserver/source/tap/CefFileIterator.java
+++ HapiServerBase/src/main/java/org/hapiserver/source/tap/CefFileIterator.java
@@ -30,7 +30,7 @@ import org.hapiserver.HapiRecord;
 import org.hapiserver.source.SourceUtil;
 
 /**
- * make CEF reader which provides records as the CEF is read in.
+ * make CEF reader which provides HapiRecords as the CEF is read in.
  *
  * @author jbf
  */
@@ -569,105 +569,7 @@ public class CefFileIterator implements Iterator<HapiRecord> {
        
         // columnIndices maps from parameter number to CEF file column(s).  If it is
         // -1, it means the data is constant, and idx will refer to the column in the CEF file.
-        return new AbstractHapiRecord() {
-            @Override
-            public int length() {
-                return columnIndices.size();
-            }
-
-            @Override
-            public String getIsoTime(int i) {
-                int idx= columnIndices.get(i).get(0);
-                String field = fields[idx].trim();
-                if (field.length() > 45) { //TODO: kludge for time ranges.  See https://github.com/hapi-server/server-java/issues/22
-                    int is1 = field.indexOf("/");
-                    if (is1 > 0) {
-                        if ( i==0 ) {
-                            field = field.substring(0, 24);
-                            if ( !field.endsWith("Z") ) field=field+"Z";
-                        } else if ( i==1 ) {
-                            field = field.substring(is1+1).substring(0,24);
-                            if ( !field.endsWith("Z") ) field=field+"Z";
-                        } else {
-                            throw new IllegalArgumentException("time ranges only supported for first two fields");
-                        }
-                    }
-                }
-                return field;
-            }
-
-            @Override
-            public String getString(int i) {
-                return getAsString(i);
-            }
-
-            @Override
-            public int getInteger(int i) {
-                return Integer.parseInt(getAsString(i));
-            }
-
-            @Override
-            public int[] getIntegerArray(int i) {
-                String[] stringArray = getStringArray(i);
-                int[] intArray = new int[stringArray.length];
-                for (int iField = 0; iField < stringArray.length; iField++) {
-                    intArray[iField] = Integer.parseInt(stringArray[iField]);
-                }
-                return intArray;
-            }
-
-            @Override
-            public double[] getDoubleArray(int i) {
-                String[] stringArray = getStringArray(i);
-                double[] doubleArray = new double[stringArray.length];
-                for (int iField = 0; iField < stringArray.length; iField++) {
-                    doubleArray[iField] = Double.parseDouble(stringArray[iField]);
-                }
-                return doubleArray;
-
-            }
-
-            @Override
-            public String[] getStringArray(int i) {
-                List<Integer> indices = columnIndices.get(i);
-                int firstIndex= indices.get(0);
-                if ( firstIndex<0 ) {
-                    return vfields.get(firstIndex);
-                } else {
-                    String[] vector = new String[indices.size()];
-                    int lastIndex= firstIndex+indices.size();
-                    for (int iField = firstIndex; iField < lastIndex; iField++) {
-                        vector[iField-firstIndex] = fields[iField].trim();
-                    }
-                    return vector;
-                }
-            }
-
-            @Override
-            public double getDouble(int i) {
-                return Double.parseDouble(getAsString(i));
-            }
-
-            @Override
-            public String getAsString(int i) {
-                int idx= columnIndices.get(i).get(0);
-                if ( idx<0 ) {
-                    return vfields.get(idx)[0];
-                } else {
-                    String s= fields[idx].trim();
-                    if ( s.startsWith("\"") && s.endsWith("\"") ) {
-                        s= s.substring(1,s.length()-1);
-                    }
-                    return s;
-                }
-            }
-
-            @Override
-            public String toString() {
-                return getAsString(0) + " " + length() + " fields";
-            }
-
-        };
+        return new CefHapiRecord(columnIndices, ffields, vfields, fields);
     }
 
     private void removeComments(ByteBuffer work_buffer, int work_size) {
