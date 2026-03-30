54b280d4d0d4126551133de26d346cda5e681610
Jeremy Faden <faden@cottagesystems.com>
2026-03-30 09:01:28 -0500

    handle resolve_references

--- HapiServer/src/java/org/hapiserver/Util.java
+++ HapiServer/src/java/org/hapiserver/Util.java
@@ -463,7 +463,7 @@ public final class Util {
      * @return the time this file was last modified.
      */
     public static String buildTime() {
-        String time= "last_modified_at: 2026-03-30T07:38";
+        String time= "last_modified_at: 2026-03-30T09:01";
         return time.substring(18);
     }
     
